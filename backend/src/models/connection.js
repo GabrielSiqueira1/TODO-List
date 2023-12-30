239a31e02ba5c09553e0dec3b7f7097d2d3bddf9
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-12-30 11:04:06 -0300

    Conexão

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-const mysql = require('mysql2/promise');
-
-require('dotenv').config();
-
-const connection = mysql.createPool({
-  host:process.env.MYSQL_HOST,
-  user:process.env.MYSQL_USER,
-  password:process.env.MYSQL_PASSWORD,
-  database:process.env.MYSQL_DB,
-=======
 // Configuração da conexão do banco de dados
 const mysql = require("mysql2/promise");
 
@@ -18,7 +7,6 @@ const connection = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
->>>>>>> 43685fa (Ajustes)
 });
 
 module.exports = connection;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-const app = require('./app');
-const dotenv = require('dotenv');
-
-const PORT = process.env.PORT || 3333;
-
-dotenv.config();
-
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`));
-=======
 // Abertura do servidor
 const app = require("./app");
 require("dotenv").config();
@@ -15,4 +5,3 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
->>>>>>> 43685fa (Ajustes)
